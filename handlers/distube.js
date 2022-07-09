38130517923bfc14bc8865f7c04950eac44dacd0
Ghost-ofc <81880988+Ghost-ofc@users.noreply.github.com>
2022-07-09 15:25:14 -0500

    Add files via upload

---
+++ handlers/distube.js
@@ -0,0 +1,51 @@
+const {DisTube, Queue, Song, default: dist} = require('distube');
+const {SpotifyPlugin} = require('@distube/spotify')
+const {SoundCloudPlugin} = require('@distube/soundcloud')
+
+module.exports = (client, Discord) => {
+
+    console.log('Modulo de musica cargado!'.red)
+    client.distube = new DisTube(client, {
+        emitNewSongOnly: false,
+        leaveOnEmpty: true,
+        leaveOnFinish: true,
+        leaveOnStop: true,
+        savePreviousSongs: true,
+        emitAddSongWhenCreatingQueue: false,
+        searchSongs: 0,
+        nsfw: false,
+        emptyCooldown: 25,
+        ytdlOptions: {
+            highWaterMark: 1024 * 1024 * 64,
+            quality: "highestaudio",
+            format: "audioonly",
+            liveBuffer: 60000,
+            dlChunkSize: 1024 * 1024 * 4,
+        },
+        youtubeDL: false,
+        plugins: [
+            new SpotifyPlugin({
+                parallel: true,
+                emitEventsAfterFetching: true,
+            }),
+            new SoundCloudPlugin()
+        ],
+    });
+
+    client.distube.on("playSong", (queue, song)=>{
+        queue.textChannel.send({
+            embeds: [new Discord.MessageEmbed()
+            .setTitle(`Escuchando \`${song.name}\` - \`${song.formatteDuration}\` `)
+            .setThumbnail(song.thumbnail)
+            .setURL(song.url)
+            .setColor("#8400ff")
+            .setFooter({text: `Añadida por ${song.user.tag}`, iconURL: song.user.displayAvatarURL({dynamic: true})})
+            ]
+        })
+    });
+
+
+    client.distube.on("initQueue", (queue) => {
+        queue.autoplay = true;
+    });
+};
---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+npm install mathjs
+npm install distube
+npm i @distube/spotify @distube/souncloud
+npm i weather-js
