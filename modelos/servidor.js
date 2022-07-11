72e64673002f69188553990922a5080363e56fd4
Ghost-ofc <81880988+Ghost-ofc@users.noreply.github.com>
2022-07-11 18:21:25 -0500

    Update servidor.js

--- modelos/servidor.js
+++ modelos/servidor.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const serverSchema = new mongoose.Schema({
     guildID: String,
     prefijo: String,
+    reaccion_roles: Array,
+    sistema_tickets: { type: Object, default: { canal: "", mensaje: "" } },
+    sugerencias: { type: String, default: "" },
+    bienvenida: { type: Object, default: { canal: "", mensaje: "", imagen: "" } },
 })
 const model= mongoose.model("ConfigServer", serverSchema);
 module.exports = model;
