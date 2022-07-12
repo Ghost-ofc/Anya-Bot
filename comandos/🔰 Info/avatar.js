c735a9acbf62e014008ab92dd683d2e821db3b1a
Ghost-ofc <81880988+Ghost-ofc@users.noreply.github.com>
2022-07-11 19:05:26 -0500

    Update avatar.js

--- comandos/🔰 Info/avatar.js
+++ comandos/🔰 Info/avatar.js
@@ -10,14 +10,14 @@ module.exports = {
         if(!member){
             const embed=new Discord.MessageEmbed()
                 .setTitle(`**Avartar de: ${message.author.tag}**`)
-                .setImage(`${message.author.avatarURL()}`, height="100", width="100")
+                .setImage(`${message.author.avatarURL()}?size=256`)
                 .setColor(0x66b3ff)
                 .setFooter(`Avatar de ${message.author.tag}`);
             message.channel.send({embeds:[embed]})
     }else{
       const embed2 = new Discord.MessageEmbed()
         .setTitle(`**Avartar de: ${member.tag}**`)
-        .setImage(`${member.avatarURL()}`, height="100", width="100")
+        .setImage(`${member.avatarURL()}?size=256`)
         .setColor(0x66b3ff)
         .setFooter(`Avatar de ${member.tag}`);
    
