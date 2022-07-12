77d09805744dd350e5569dbe2efe284b77849df2
Ghost-ofc <81880988+Ghost-ofc@users.noreply.github.com>
2022-07-11 22:16:15 -0500

    Add files via upload

---
+++ modelos/servidor.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const serverSchema = new mongoose.Schema({
+    guildID: String,
+    prefijo: String,
+    reaccion_roles: Array,
+    sistema_tickets: { type: Object, default: { canal: "", mensaje: "" } },
+    sugerencias: { type: String, default: "" },
+    bienvenida: { type: Object, default: { canal: "", mensaje: "", imagen: "" } },
+})
+const model= mongoose.model("ConfigServers", serverSchema);
+module.exports = model;
