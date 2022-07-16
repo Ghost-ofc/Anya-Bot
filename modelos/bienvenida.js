6fa5dd2ef2f8a1129321c54b666e351a89791d21
Ghost-ofc <81880988+Ghost-ofc@users.noreply.github.com>
2022-07-15 19:13:45 -0500

    Delete modelos directory

--- modelos/bienvenida.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const setupSchema = new mongoose.Schema({
-    guildID: String,
-    prefijo: String,
-    bienvenida: { type: Object, default: { canal: "", mensaje: "", imagen: "" } },
-
-})
-const model= mongoose.model("Bienvenida", setupSchema);
-module.exports = model;
--- modelos/servidor.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const serverSchema = new mongoose.Schema({
-    guildID: String,
-    prefijo: String,
-
-})
-const model= mongoose.model("ConfigServers", serverSchema);
-module.exports = model;
