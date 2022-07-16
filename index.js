ae2759cd8f2020ddcae5f07568db3738ba06a271
Ghost-ofc <81880988+Ghost-ofc@users.noreply.github.com>
2022-07-16 16:47:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,34 @@ client.on("ready", () => {
     }, 30000)
 })
 
+const { WelcomeCard } = require('naotori'), { MessageAttachment } = require('discord.js')
+
+client.on('guildMemberAdd', async (member) => {
+   
+     
+    const embed = new Discord.MessageEmbed()
+    .setColor("#0ff12d")
+    .setTitle(`Bienvenido a Ghost Pro`)
+    .setDescription(`¡Bienvenido  __**${member.user.username}**__ a Ghost Pro\n\n ➡ Relacionate con toda la cominidad y tener Amigos :3 <#894680299236122637>`)
+    .setThumbnail(member.user.displayAvatarURL())
+    .setFooter({text: `Gracias por unirte :D`,  IconURL: `https://i.imgur.com/CL9Fkoy.jpeg` })
+    .setTimestamp();
+
+    let wlcCarc = new WelcomeCard()
+    .setBackground('https://i.imgur.com/CL9Fkoy.jpeg')
+    .setMemberIcon(member.user.displayAvatarURL({ format: 'png', size: 2048 }))
+    .setTitle(`Bienvenido ${member.user.username}`)
+    .setDescription('Pasatela bien en este server!')
+    .setCircleColor('#000000')
+    .setFont('default')
+
+    const card = await wlcCarc.render()
+    const welcome = new MessageAttachment(card, 'welcome.png')
+    ///// envia el embed :D
+    client.channels.cache.get('996200688910614730').send({ embeds: [embed],  content: `**Hola ${member}!** **recuerda pasartela bien**` })
+    client.channels.cache.get('996200688910614730').send({ files: [welcome], })
+})
+
 client.on("guildCreate", (guild) => {
    
   const canalinv = guild.channels.cache.find(canal => canal.type === 'GUILD_TEXT' && canal.permissionsFor(guild.me).has('SEND_MESSAGES'))
