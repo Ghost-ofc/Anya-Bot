1af9c872f14881fac9d9ad770e639c6002f24ad8
Ghost-ofc <81880988+Ghost-ofc@users.noreply.github.com>
2022-07-17 01:26:32 -0500

    Update setups.js

--- modelos/setups.js
+++ modelos/setups.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 const setupSchema = new mongoose.Schema({
     guildID: String,
     reaccion_roles: Array,
-    sistema_tickets: {type: Object, default: {canal: "", mensaje: ""}},
-    sugerencias: {type: String, default: ""},
+    sistema_tickets: { type: Object, default: { canal: "", mensaje: "" } },
+    sugerencias: { type: String, default: "" },
+    bienvenida: { type: Object, default: { canal: "", mensaje: "", imagen: "" } },
 })
 
 const model = mongoose.model("Configuraciones", setupSchema);
