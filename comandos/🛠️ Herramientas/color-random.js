97fca3a0a92dc60883a61980bacd1c8e12101d1e
Ghost-ofc <81880988+Ghost-ofc@users.noreply.github.com>
2022-07-17 00:41:55 -0500

    Delete comandos directory

--- comandos/🛠️ Herramientas/color-random.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: "color-random",
-    aliases: ["randomcolor", "colorrandom", "color-aleatorio"],
-    description: "Generador de Color aleatorio",
-    run: async (client, message, args, prefix) => {
-          let coloraleatorio = '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
-          message.channel.send(`Color Generado: \`${coloraleatorio}\``)
-    }
-  }
--- comandos/🛠️ Herramientas/ram.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: "ram",
-    desc: "Mira cuanta RAM está usando el bot",
-     run: async (client, message, args, cmduser, text, prefix) => {
-     message.channel.send(`Estoy usando ${(process.memoryUsage().heapUsed/1024/1024).toFixed(2)} MB de RAM`)
-    }
-  };
