41dd55ce4b711829abc44ef6841327056c2b2a17
Ghost-ofc <81880988+Ghost-ofc@users.noreply.github.com>
2022-07-29 00:27:49 -0500

    Update say.js

--- comandos/🚶 Rol/say.js
+++ comandos/🚶 Rol/say.js
@@ -8,8 +8,6 @@ module.exports = {
         
         const mensaje = args.join(" ")
         if(!mensaje) return message.reply(":triangular_flag_on_post: **Debes especificar un mensaje para comentar.**")
-        let permiso = message.member.permissions.has("MUTE_MEMBERS")
-        if(!permiso) return message.channel.send(":triangular_flag_on_post: **Solo los STAFF de este servidor pueden ejecutar este comando.**")
 
         setTimeout(function(){
             message.delete()
