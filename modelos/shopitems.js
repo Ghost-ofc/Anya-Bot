81c67a7d15e9f60b6de4afd890e453bff4a1f6d0
Ghost-ofc <81880988+Ghost-ofc@users.noreply.github.com>
2022-08-06 23:47:08 -0500

    Add files via upload

---
+++ modelos/shopitems.js
@@ -0,0 +1,32 @@
+module.exports = [
+    {
+      item: "pizza",
+      price: 2500,
+      emoji: ":pizza:"
+    },
+    {
+      item: "banana",
+      price: 443,
+      emoji: ":banana:"
+    },
+    {
+      item: "lipstick",
+      price: 463,
+      emoji: ":lipstick:"
+    },
+    {
+      item: "phone",
+      price: 4000,
+      emoji: ":iphone:"
+    },
+    {
+      item: "pc",
+      price: 12000,
+      emoji: ":desktop:"
+    },
+    {
+      item: "laptop",
+      price: 12000,
+      emoji: ":computer:"
+    },
+  ]
