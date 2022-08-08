525a9f487d875ff9ede456c50d5e07d804c63fc8
Ghost-ofc <81880988+Ghost-ofc@users.noreply.github.com>
2022-08-07 22:03:08 -0500

    Update serverinfo.js

--- comandos/🔰 Info/serverinfo.js
+++ comandos/🔰 Info/serverinfo.js
@@ -1,4 +1,39 @@
-.addFields(
+const Discord = require('discord.js');
+const day = require("dayjs")
+
+module.exports = {
+  name: "server-info", 
+  alias: ['server', 'serverinfo'], 
+
+run: async (client, message, args) => {
+    
+    const f = message.guild.ownerId;
+        
+    const createsv = day(message.guild.createdAt).format('DD/MM/YY');
+
+let seguridad; //NONE, LOW, MEDIUM , HIGH , VERY_HIGH
+const perm = message.guild.verificationLevel;
+if(perm === "NONE"){
+seguridad = "No Hay";
+}
+if(perm === "LOW"){
+seguridad = "Bajo";
+};
+if(perm === "MEDIUM"){
+seguridad = "Medio";
+};
+if(perm === "HIGH"){
+seguridad = "Alto";
+};
+if(perm === "VERY_HIGH"){
+seguridad = "MUY ALTO";
+};
+        
+    const serv = new Discord.MessageEmbed()
+    .setAuthor(message.guild.name, message.guild.iconURL())
+    .setThumbnail(message.guild.iconURL())
+    .setColor(`#a51400`)
+    .addFields(
       {
         name: `<:ServerOwner:981456566073053235> **Owner**`,
         value: `<@${message.guild.ownerId}>`,
@@ -44,3 +79,8 @@
         value: `**[${guild.roles.cache.size}]** roles`
       }
     )
+    message.channel.send({embeds: [serv]})
+
+ }
+
+} 
