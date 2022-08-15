02384f722ae370b188790c79331a9d27d5cdeed9
Ghost-ofc <81880988+Ghost-ofc@users.noreply.github.com>
2022-08-14 19:19:59 -0500

    Update furro.js

--- comandos/🚶 Rol/furro.js
+++ comandos/🚶 Rol/furro.js
@@ -7,10 +7,12 @@ module.exports = {
 
     run: async (client, message, args, prefix) => {
         let furro = Math.floor(Math.random() * 100)
+        
+        const user = message.guild.members.cache.get(args[0]) || message.mentions.members.first() || message.member;
 
         const embed = new Discord.MessageEmbed()
 
-        .setTitle(`${message.author.tag} **Tu nivel de furro es de** ${furro}%`)
+        .setTitle(`${user.user.tag} **Tu nivel de furro es de** ${furro}%`)
         .setColor("RANDOM")
         .setImage("https://i.pinimg.com/736x/ee/20/19/ee201936dd277945646648d175aefa9a.jpg")
         .setTimestamp()
