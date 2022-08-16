3c9f7358fcdeddda83e57212701a3900ee2a42e3
Ghost-ofc <81880988+Ghost-ofc@users.noreply.github.com>
2022-08-16 13:43:27 -0500

    Add files via upload

---
+++ comandos/🛠️ Herramientas/searchpinterest.js
@@ -0,0 +1,31 @@
+const {MessageAttachment, Util} = require('discord.js');
+const axios = require('axios');
+const Discord = require('discord.js')
+
+module.exports = {
+    name: "searchpinterest",
+    aliases: ["search-pinterest", "pinterest"],
+    desc: "Buscar informacion sobre un perfil de instagram",
+    permisos: ["ADMINISTRATOR"],
+    owner: false,
+    run: async(client, message, args, prefix) => {
+
+        const foto1 = args[0]
+		if(!args[0]) return message.reply(" **Tienes que especificar un usuario** ")
+
+        try {
+			let foto = `https://leyscoders-api.herokuapp.com/api/pinsearch?q=${foto1}&apikey=MIMINGANZ`;
+
+            const foto2 = new MessageAttachment(foto, 'Pinterest.png');
+				
+				message.reply({
+                    content: 'Imagen encontrada.',
+                    files: [foto2],
+                });
+				
+			
+		} catch(e){
+            console.warn(e)
+          }
+	}
+}
