847af16655ee8cd080f4669fb23288c434598c82
Juan Gutierrez <81880988+Ghost-ofc@users.noreply.github.com>
2023-01-14 11:07:42 -0500

    Update dni.js

--- comandos/🌀 Premium/dni.js
+++ comandos/🌀 Premium/dni.js
@@ -1,8 +1,11 @@
-const { Client, Message, MessageEmbed } = require('discord.js');
+const { MessageAttachment, MessageEmbed } = require('discord.js');
 const axios = require('axios');
+const { spawn } = require('child_process');
+const fs = require('fs');
+const { data } = require('jquery');
 
 module.exports = {
-    name: "dni",
+    name: "dnifo",
     aliases: ["DNI"],
     desc: "Realizar un doxeo basico",
     owner: false,
@@ -14,7 +17,6 @@ module.exports = {
 
         /*axios.get(`https://consulta.api-peru.com/api/dni/${DNI}/52dcc153f3d129c6bb8e78a0a5380887`).then(res => {
             let {numero, nombres, apellido_paterno, apellido_materno, codigo_verificacion, sexo, fecha_nacimiento, fecha_actualizacion, ubigeo_domicilio, direccion , ubigeo_reniec, ubigeo} = res.data
-
             const embed = new MessageEmbed()
             .setTitle('Resultado de su busqueda: ')
             .setDescription("Si ")
@@ -28,17 +30,35 @@ module.exports = {
             .setColor("DARK_AQUA")
             message.channel.send({ embeds: [embed] });
         })*/
+        var routepython = "./script/doxeo2.py";
+        var process = spawn('python',[ routepython, `${DNI}`] );  
+    
+        process.stdout.on('data', function(data) {
+        data = data.toString('utf8'); 
+        
+        const base64Data = data;
+        //console.log(base64Data)
+        const buffer = Buffer.from(base64Data, 'base64');
 
+        fs.writeFile(`./fotos/${DNI}.png`, buffer, (err) => {
+            if (err) throw err;
+            //console.log('The file has been saved!');
+        });
 
+        } )
+
+        setTimeout(() => {
         axios.get(`https://citasenlinea.sisol.gob.pe/Account/FnGetPaciente?idTipoDocumento=1&nroDocumento=${DNI}`
         ).then(res => {
 
             if(res.data == null) return message.reply("❌ **No se ha encontrado el dni**");
             let {numDocumento, noNombres, apePaterno, apeMaterno, idUbigeoReniec, deDireccion, idSexo, feNacimiento} = res.data
-
+            const file = new MessageAttachment(`./fotos/${DNI}.png`);
             const embed = new MessageEmbed()
             .setTitle('Resultado de su busqueda: ')
             .setDescription("USE ESTA INFORMACION CON CUIDADO - EL DUEÑO DEL BOT NO SE HACE RESPONSABLE POR EL MAL USO QUE SE LE DE ")
+            
+            .setImage(`attachment://${DNI}.png`)
             .addFields(
                 {name: '**Nombre**', value: `**${noNombres}**`, inline: true},
                 {name: '**Apellido Paterno**', value: `**${apePaterno}**`, inline: true},
@@ -50,8 +70,8 @@ module.exports = {
                 {name: '**SEXO**', value: `**${idSexo}**`, inline: true}
             )
             .setColor("DARK_AQUA")
-            message.channel.send({ embeds: [embed] });
+            message.channel.send({ embeds: [embed], files: [file] });
         })
-    
+    }, 8000);
     }
 }
